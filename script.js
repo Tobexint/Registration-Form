87e8f05ebeaa0164e027ed15e1bc4c16ae8d3cce
Tobex <89360416+Tobexint@users.noreply.github.com>
2023-06-19 19:21:22 -0700

    DOM manipulation effects with JavaScript.

--- script.js
+++ script.js
@@ -6,3 +6,28 @@ document.querySelector('#root').innerHTML = message
 
 // Log to the console
 console.log(message)
+
+var button = document.getElementById('button').addEventListener('click', buttonClick);
+
+var t = document.querySelector('.container')
+
+t.addEventListener('mouseenter', runEvent);
+t.addEventListener('mouseleave', leave);
+
+function buttonClick(){
+    console.log("Button Clicked!");
+    document.querySelector('.container').style.backgroundColor = "lightgray";
+    
+}
+
+function runEvent(){
+    document.querySelector('.container').style.backgroundColor = "skyblue";
+}
+
+function leave(){
+    document.querySelector('.container').style.backgroundColor = "white";
+}
+
+function enter(){
+    document.querySelector('input[type="text"]').style.backgroundColor = "gray";
+}
